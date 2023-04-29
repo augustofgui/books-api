3f652987369d58d6bc90831f28b30a454f0ec394
Augusto Ferreira Guilarducci <augustofgui@gmail.com>
2023-04-29 12:59:18 -0300

    feat: create initial express server setup

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Response } from 'express';
+const app = express()
+
+const port = 3000
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
