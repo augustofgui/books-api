8670ef76e2264eccb0e8d71d85be760c1c8f8384
Augusto Ferreira Guilarducci <augustofgui@gmail.com>
2023-04-29 23:39:08 -0300

    feat: remove "powered" from header
    This feature is used in order to prevent attacks
    from simple scraping programs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express, { Response } from "express";
-import { routes } from "./routes/routes";
+import express, { Response } from 'express';
+import { routes } from './routes/routes';
 
 const app = express();
 
+app.disable('x-powered-by');
+
 const port = 3000;
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
