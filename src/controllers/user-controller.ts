fb9e11eb9503b9d897ef178436eca330adc6ed85
Augusto Ferreira Guilarducci <augustofgui@gmail.com>
2023-04-30 00:24:57 -0300

    feat: create user dto for controller resquest body with zod

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,13 @@
+import { Request, Response } from 'express';
+import { createUserBody } from '../utils/dto/create-user-body';
+import { User } from '../entities/user';
+
 export default class UserController {
-  async create() {
-    console.log("oi");
+  async create(request: Request, response: Response) {
+    const { name, email, password } = createUserBody.parse(request.body);
+
+    const user = new User({ name, email, password });
+
+    return response.status(201).json({ user });
   }
 }
